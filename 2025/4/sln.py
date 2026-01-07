9c987167ec882f466ea5c06e13a2cfb3db6451d9
Nicholas Spihlman <nspihlman@gmail.com>
2026-01-07 15:59:40 -0500

    2025aoc day 4 part 2

--- 2025/4/sln.py
+++ 2025/4/sln.py
@@ -2,8 +2,10 @@
 def valid_coordinates(row, col, mp):
     return 0 <= row < len(mp) and 0 <= col < len(mp[row])
 
+
 def is_paper_roll(row, col, mp):
-    return mp[row][col] == '@'
+    return mp[row][col] == '@' or mp[row][col] == 'x'
+
 
 def check_neighbor_rolls(row: int, col: int, mp: list[str]) -> bool:
     # check if total neighbor rolls is < 4
@@ -18,12 +20,31 @@ def check_neighbor_rolls(row: int, col: int, mp: list[str]) -> bool:
             total += 1
     return total < 4
 
-def find_accessible_rolls(mp: list[str]) -> int:
-    total = 0
+
+def mark_for_change(row, col, mp):
+    mp[row] = mp[row][:col] + 'x' + mp[row][col+1:]
+
+
+def remove_marked_rolls(mp):
     for row in range(len(mp)):
         for col in range(len(mp[row])):
-            if is_paper_roll(row, col, mp) and check_neighbor_rolls(row, col, mp):
-                total += 1
+            if mp[row][col] == 'x':
+                mp[row] = mp[row][:col] + '.' + mp[row][col+1:]
+
+
+def find_accessible_rolls(mp: list[str]) -> int:
+    total = 0
+    while True:
+        removed = 0
+        for row in range(len(mp)):
+            for col in range(len(mp[row])):
+                if is_paper_roll(row, col, mp) and check_neighbor_rolls(row, col, mp):
+                    removed += 1
+                    mark_for_change(row, col, mp)
+        if removed == 0:
+            break
+        total += removed
+        remove_marked_rolls(mp)
     return total
 
 def main():
