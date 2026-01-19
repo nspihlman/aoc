ced07ca575b5fa03226afc4461bcacad2a97adbb
Nicholas Spihlman <nspihlman@gmail.com>
2026-01-19 15:36:18 -0500

    Working on aoc2025 day 8 part1

--- 2025/6/sln.py
+++ 2025/6/sln.py
@@ -9,6 +9,39 @@ def find_grand_total(nums: list[list[int]], ops: list[str]) -> int:
             total = total + (nums[0][i] * nums[1][i] * nums[2][i] * nums[3][i])
     return total
 
+def add_0s_to_num(num: str, to_add: int) -> str:
+    if to_add == 0:
+        return num
+    else:
+        num =  num + ("0" * to_add)
+    return num
+
+def make_nums_same_length(num1: str, num2: str, num3: str, num4) -> tuple[str, str, str, str]:
+    max_length = max(len(num1), max(len(num2), len(num3)))
+    num1 = add_0s_to_num(num1, len(num1) - max_length)
+    num2 = add_0s_to_num(num2, len(num2) - max_length)
+    num3 = add_0s_to_num(num3, len(num3) - max_length)
+    num4 = add_0s_to_num(num4, len(num4) - max_length)
+    values = []
+    for j in range(len(num1) - 1, -1, -1):
+        value = (num1[j] * 1000) + (num2[j] * 100) + (num3[j] * 10)
+    return num1, num2, num3, num4
+
+def find_grand_total_pt2(nums: list[list[int]], ops: list[str]) -> int:
+    total = 0
+    for i in range(len(ops)):
+        num1 = str(nums[i][0])
+        num2 = str(nums[i][1])
+        num3 = str(nums[i][2])
+        num4 = str(nums[i][3])
+        num1, num2, num3 = make_nums_same_length(num1, num2, num3, num4)
+
+        for j in range(len(num1) - 1, -1, -1):
+            v
+            if ops[i] == '+':
+                total += num1[j] + num2[j] + num3[j] + num4[j]
+
+
 def main():
     lines = []
     operations = []
@@ -21,7 +54,8 @@ def main():
                 lines.append([int(x) for x in vals])
             else:
                 operations = vals
-    print(find_grand_total(lines, operations))
+    #print(find_grand_total(lines, operations))
+    print(find_grand_total_pt2(lines, operations))
 
 if __name__ == '__main__':
     main()
---
+++ 2025/8/sln.py
@@ -0,0 +1,84 @@
+from math import sqrt
+
+class Point:
+    def __init__(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.connected = []
+
+
+def are_connected(p: Point, q: Point):
+    return q in p.connected
+
+
+def connect_points(p: Point, q: Point):
+    for point in p.connected:
+        if q not in point.connected and q != point:
+            point.connected.append(q)
+        if point not in q.connected and point != q:
+            q.connected.append(point)
+    p.connected.append(q)
+    q.connected.append(p)
+
+
+def calculate_distance(p: Point, q: Point) -> float:
+    return sqrt(pow(p.x - q.x, 2) + pow(p.y - q.y, 2) + pow(p.z - q.z, 2))
+
+
+def multiply_circuits(points: list[Point]) -> int:
+    circ1 = 0
+    circ1_mem = []
+    circ2 = 0
+    circ2_mem = []
+    circ3 = 0
+    circ3_mem = []
+    for p in points:
+        if len(p.connected) > circ1 and p not in circ1_mem:
+            circ3 = circ2
+            circ3_mem = circ2_mem
+            circ2 = circ1
+            circ2_mem = circ1_mem
+            circ1 = len(p.connected)
+            circ1_mem = p.connected
+        elif len(p.connected) > circ2 and p not in circ2_mem:
+            circ3 = circ2
+            circ3_mem = circ2_mem
+            circ2 = len(p.connected)
+            circ2_mem = p.connected
+        elif len(p.connected) > circ3 and p not in circ3_mem:
+            circ3 = len(p.connected)
+            circ3_mem = p.connected
+    return circ1 * circ2 * circ3
+
+def main():
+    all_points: list[Point] = []
+    distances: list[tuple[float, Point, Point]] = []
+    with open('tmp.txt') as f:
+        for line in f:
+            vals = [int(x) for x in line.split(',')]
+            all_points.append(Point(vals[0], vals[1], vals[2]))
+
+    for i in range(len(all_points)):
+        p = all_points[i]
+        for j in range(i+1, len(all_points)):
+            q = all_points[j]
+            distances.append((calculate_distance(p, q), p, q))
+    distances = sorted(distances, key=lambda x: x[0])
+    total_ops = 10
+    cur_op = 0
+    d_place = 0
+    while cur_op < total_ops:
+        d = distances[d_place]
+        if are_connected(d[1], d[2]) or are_connected(d[2], d[1]):
+            d_place += 1
+            continue
+        else:
+            connect_points(d[1], d[2])
+            cur_op += 1
+            d_place += 1
+    print(multiply_circuits(all_points))
+
+
+if __name__ == '__main__':
+    main()
